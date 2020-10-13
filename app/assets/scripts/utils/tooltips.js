2e3aa671f5868321a14dc746049454a50fa8dc9a
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2020-10-13 15:51:12 +0200

    update sources attribute display due to migration

--- app/assets/scripts/utils/tooltips.js
+++ app/assets/scripts/utils/tooltips.js
@@ -4,5 +4,5 @@ export function indicatorTooltipContent (indicator, lang) {
   const defaultDescriptionLabel = t.description_default_value;
   const indicatorDescription = lang === 'en' ? indicator.description : indicator.description_ar;
   return `<span class="tooltip__description">${t.description_label} : &nbsp; ${indicatorDescription !== null ? indicatorDescription : defaultDescriptionLabel}</span>` +
-    `${(indicator.sources) ? `<span class="tooltip__sources">Sources: ${indicator.sources.join(', ')}</span>` : ''}`;
+    `${(indicator.sources) ? `<span class="tooltip__sources">Sources: ${indicator.sources.map(item => item[lang])}</span>` : ''}`;
 }
